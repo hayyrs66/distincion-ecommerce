22234e8d740133cc2a94c58c235bde1f79f8630e
hayyrs66 <rayrtsx@proton.me>
2024-11-16 13:38:15 -0600

    add files

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="w-full fixed top-0 left-0 z-10 py-4 px-8">
+      <h1 className="text-6xl tracking-tight font-medium text-white">
+        Distinto
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="w-full h-screen bg-hero-image bg-no-repeat bg-cover bg-center">
+      
+    </section>
+  );
+};
+
+export default Hero;
