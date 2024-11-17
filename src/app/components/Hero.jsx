9896273c4422b685a896fab5161df6d30c1d98a6
hayyrs66 <rayrtsx@proton.me>
2024-11-16 20:45:06 -0600

    refactor Hero component layout and integrate Showcase component

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Showcase from "./Showcase";
 
 const Hero = () => {
   return (
-    <section className="w-full h-screen bg-hero-image bg-no-repeat bg-cover bg-center">
-      
+    <section className="">
+      <div className="w-full h-screen max-h-[550px] bg-hero-image bg-no-repeat bg-cover bg-top px-8 py-36">
+        <div className="flex flex-col">
+          <h2 className="text-white/80 text-2xl font-medium">
+            Tu Próximo Pantalón, Aquí
+          </h2>
+        </div>
+      </div>
+      <Showcase />
     </section>
   );
 };
