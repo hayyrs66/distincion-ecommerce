1b4254df5ce9a9369063630591d9951349809b3a
hayyrs66 <rayrtsx@proton.me>
2024-11-25 21:49:24 -0600

    update Hero component styles for improved layout and typography

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,10 +4,10 @@ import Showcase from "./Showcase";
 const Hero = () => {
   return (
     <section className="">
-      <div className="w-full h-screen max-h-[550px] bg-hero-image bg-no-repeat bg-cover bg-top px-8 py-36">
+      <div className="w-full h-screen max-h-[550px] bg-hero-image bg-no-repeat bg-cover bg-top px-8 py-44">
         <div className="flex flex-col">
-          <h2 className="text-white/80 text-2xl font-medium">
-            Tu Próximo Pantalón, Aquí
+          <h2 className="text-white text-7xl max-w-[800px] text-balance font-medium">
+            Tu Próximo Pantalón Aquí.
           </h2>
         </div>
       </div>
