671e798341376924aee6d062428f500c70da9b71
hayyrs66 <rayrtsx@proton.me>
2024-11-27 09:01:10 -0600

    added new component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Hero/>  
+        <About/>
       </main>
     </div>
   );
