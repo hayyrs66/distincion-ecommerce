5185b573e4348c1ec6400ed857720ba6701be7cf
hayyrs66 <rayrtsx@proton.me>
2024-11-30 16:19:22 -0600

    Refactor CartItem component for improved styling and layout

--- src/app/components/CartItem.jsx
+++ src/app/components/CartItem.jsx
@@ -4,13 +4,12 @@ import Trash from "./icons/Trash";
 import Up from "./icons/Up";
 
 const CartItem = ({ item, onIncrease, onDecrease, onRemove }) => {
-
   useEffect(() => {
     console.log("CartItem render, item:", item);
   }, []);
-  
+
   return (
-    <article className="border-b p-4 rounded-md flex justify-between items-center">
+    <article className="border-b border-b-black/10 p-4 rounded-md flex justify-between items-center">
       {/* Detalles del Producto */}
       <div className="flex gap-4 items-center">
         <div>
@@ -26,7 +25,9 @@ const CartItem = ({ item, onIncrease, onDecrease, onRemove }) => {
           <h5 className="text-base font-semibold">{item.name}</h5>
           <p className="text-sm text-gray-500">Q{item.price.toFixed(2)}</p>
           <div className="flex items-center gap-2 mt-1">
-            <p className="text-sm">{item.size} - {item.color}</p>
+            <p className="text-sm">
+              {item.size} - {item.color}
+            </p>
             <div
               className={`w-4 h-4 rounded-full border border-gray-300 flex items-center justify-center`}
             >
@@ -44,15 +45,15 @@ const CartItem = ({ item, onIncrease, onDecrease, onRemove }) => {
         <div className="flex items-center">
           <button
             onClick={onDecrease}
-            className="bg-gray-200 rounded-md text-gray-700"
+            className="bg-gray-200 rounded-md hover:bg-gray-300 transition-colors active:bg-green-200"
             aria-label="Disminuir cantidad"
           >
             <Down />
           </button>
-          <span className="px-4 text-sm">{item.quantity}</span>
+          <span className="px-2 text-sm">{item.quantity}</span>
           <button
             onClick={onIncrease}
-            className="bg-gray-200 rounded-md text-gray-700"
+            className="bg-gray-200 rounded-md hover:bg-gray-300 transition-colors active:bg-green-200"
             aria-label="Aumentar cantidad"
           >
             <Up />
@@ -60,7 +61,7 @@ const CartItem = ({ item, onIncrease, onDecrease, onRemove }) => {
         </div>
         <button
           onClick={onRemove}
-          className="text-red-500 hover:text-red-700"
+          className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md"
           aria-label="Eliminar artículo"
         >
           <Trash />
