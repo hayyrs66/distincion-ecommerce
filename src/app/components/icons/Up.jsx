96239443e00ed6316a1b6725b76b985a1bb607ed
hayyrs66 <rayrtsx@proton.me>
2024-11-30 16:20:09 -0600

    Reduce Up icon size from 34x34 to 27x27 for better alignment

--- src/app/components/icons/Up.jsx
+++ src/app/components/icons/Up.jsx
@@ -4,8 +4,8 @@ const Up = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="34"
-      height="34"
+      width="27"
+      height="27"
       viewBox="0 0 24 24"
       className="fill-gray-500"
     >
