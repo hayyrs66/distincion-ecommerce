03504fa2260761727ad4e77cdb69166092c182fd
hayyrs66 <rayrtsx@proton.me>
2024-11-30 15:46:49 -0600

    Fix link path in Hero component for correct navigation

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
             Tu Próximo Pantalón Aquí.
           </h2>
           <Link
-            href="ropa"
+            href="/ropa"
             className="flex items-center gap-2 text-black font-semibold text-xl bg-white w-fit px-4 py-2 rounded-md"
           >
             Compra y Explora
