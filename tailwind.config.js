96f7d289f954e241d6ac2f2387b44d9051c1dbe5
hayyrs66 <rayrtsx@proton.me>
2024-11-30 17:27:23 -0600

    Enable dark mode support and enhance Tailwind configuration with new animations and responsive design settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,30 +9,45 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-poppins)']
+        sans: ["var(--font-poppins)"],
       },
       backgroundImage: {
         "hero-image": "url('/assets/images/santi.avif')",
         "about-image": "url('/assets/images/santi.jpg')",
       },
-        keyframes:{
-          smallFont:{
-            "0%": {fontSize: "8rem"},         
-            "100%": {fontSize: "3rem"},         
+
+      keyframes: {
+        smallFont: {
+          "0%": {
+            fontSize: "8rem",
           },
-          bigFont:{
-            "0%": {fontSize: "3rem"},         
-            "100%": {fontSize: "8rem"},         
+          "100%": {
+            fontSize: "3rem",
           },
         },
-        animation:{
-          smallFont: "smallFont 400ms cubic-bezier(.23,.78,.63,.99) forwards",
-          bigFont: "bigFont 400ms cubic-bezier(.23,.78,.63,.99) forwards",
+        bigFont: {
+          "0%": {
+            fontSize: "3rem",
+          },
+          "100%": {
+            fontSize: "8rem",
+          },
         },
-        gridTemplateColumns: {
-          "clothes-section": "1fr 0.5fr"
-        }
+      },
+      animation: {
+        smallFont: "smallFont 400ms cubic-bezier(.23,.78,.63,.99) forwards",
+        bigFont: "bigFont 400ms cubic-bezier(.23,.78,.63,.99) forwards",
+      },
+      gridTemplateColumns: {
+        "clothes-section": "1fr 0.5fr",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
