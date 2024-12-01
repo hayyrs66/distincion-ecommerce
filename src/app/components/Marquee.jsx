feefdfb78184d286434f674031a14e1e7045c7ae
hayyrs66 <rayrtsx@proton.me>
2024-11-30 18:37:19 -0600

    Refactor Marquee component for improved layout and remove unused InfiniteMarquee

--- src/app/components/Marquee.jsx
+++ src/app/components/Marquee.jsx
@@ -1,34 +1,43 @@
-import InfiniteMarquee from "vanilla-infinite-marquee";
-import { useEffect } from "react";
 import "../../../node_modules/vanilla-infinite-marquee/infinite-marquee.css";
 import Link from "next/link";
 
 const Marquee = () => {
-  useEffect(() => {
-    const marquee = new InfiniteMarquee({
-      element: ".marquee-container",
-      speed: 19000,
-      smoothEdges: false,
-      direction: "left",
-      duplicateCount: 2,
-      pauseOnHover: true,
-      mobileSettings: {
-        direction: "left",
-        speed: 200000,
-      },
-    });
-
-    return () => {
-      marquee.destroy();
-    };
-  }, []);
-
   return (
-    <div className="marquee-container bg-black">
-      <div className="flex mx-2 items-center">
+    <div className="bg-black">
+      <div className="flex px-2 gap-2 items-center">
         <Link
           href="/"
-          className="font-semibold bg-gradient-to-r from-white  to-indigo-200 inline-block text-transparent bg-clip-text"
+          className="font-semibold text-white overflow-hidden text-nowrap inline-block"
+        >
+          COLECCIÓN PLAYERAS 2025{" "}
+        </Link>
+        <Link
+          href="/"
+          className="font-semibold text-white overflow-hidden text-nowrap inline-block"
+        >
+          COLECCIÓN PLAYERAS 2025{" "}
+        </Link>
+        <Link
+          href="/"
+          className="font-semibold text-white overflow-hidden text-nowrap inline-block"
+        >
+          COLECCIÓN PLAYERAS 2025{" "}
+        </Link>
+        <Link
+          href="/"
+          className="font-semibold text-white overflow-hidden text-nowrap inline-block"
+        >
+          COLECCIÓN PLAYERAS 2025{" "}
+        </Link>
+        <Link
+          href="/"
+          className="font-semibold text-white overflow-hidden text-nowrap inline-block"
+        >
+          COLECCIÓN PLAYERAS 2025{" "}
+        </Link>
+        <Link
+          href="/"
+          className="font-semibold text-white overflow-hidden text-nowrap inline-block"
         >
           COLECCIÓN PLAYERAS 2025{" "}
         </Link>
