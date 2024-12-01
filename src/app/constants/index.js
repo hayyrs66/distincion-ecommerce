681ceeddc28a7e02fcbc12e73e93de06cdcc0302
hayyrs66 <rayrtsx@proton.me>
2024-11-30 18:37:26 -0600

    Remove unused image arrays from pantalones_general constants for cleaner code

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -5,42 +5,6 @@ export const pantalones_general = [
     nombre: "Pantalón Ajustado",
     precio: 320,
     imagen: "assets/fotos/persona_1.avif",
-    imagenes: [
-      {
-        azul: [
-          "assets/fotos/ajustado/AJUSTADO_AZUL_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_AZUL_ATRAS.avif",
-        ],
-        besh: [
-          "assets/fotos/ajustado/AJUSTADO_BESH_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_BESH_ATRAS.avif",
-        ],
-        cocoa: [
-          "assets/fotos/ajustado/AJUSTADO_COCOA_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_COCOA_ATRAS.avif",
-        ],
-        gris_claro: [
-          "assets/fotos/ajustado/AJUSTADO_GRIS_CLARO_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_GRIS_CLARO_ATRAS.avif",
-        ],
-        gris_oscuro: [
-          "assets/fotos/ajustado/AJUSTADO_GRIS_OSCURO_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_GRIS_OSCURO_ATRAS.avif",
-        ],
-        kaki: [
-          "assets/fotos/ajustado/AJUSTADO_KAKI_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_KAKI_ATRAS.avif",
-        ],
-        negro: [
-          "assets/fotos/ajustado/AJUSTADO_NEGRO_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_NEGRO_ATRAS.avif",
-        ],
-        verde: [
-          "assets/fotos/ajustado/AJUSTADO_VERDE_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_VERDE_ATRAS.avif",
-        ],
-      },
-    ],
   },
   {
     id: 2,
@@ -48,42 +12,6 @@ export const pantalones_general = [
     nombre: "Pantalón Ajustado",
     precio: 320,
     imagen: "assets/fotos/ajustado/AJUSTADO_NEGRO_FRENTE.avif",
-    imagenes: [
-      {
-        azul: [
-          "assets/fotos/ajustado/AJUSTADO_AZUL_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_AZUL_ATRAS.avif",
-        ],
-        besh: [
-          "assets/fotos/ajustado/AJUSTADO_BESH_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_BESH_ATRAS.avif",
-        ],
-        cocoa: [
-          "assets/fotos/ajustado/AJUSTADO_COCOA_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_COCOA_ATRAS.avif",
-        ],
-        gris_claro: [
-          "assets/fotos/ajustado/AJUSTADO_GRIS_CLARO_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_GRIS_CLARO_ATRAS.avif",
-        ],
-        gris_oscuro: [
-          "assets/fotos/ajustado/AJUSTADO_GRIS_OSCURO_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_GRIS_OSCURO_ATRAS.avif",
-        ],
-        kaki: [
-          "assets/fotos/ajustado/AJUSTADO_KAKI_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_KAKI_ATRAS.avif",
-        ],
-        negro: [
-          "assets/fotos/ajustado/AJUSTADO_NEGRO_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_NEGRO_ATRAS.avif",
-        ],
-        verde: [
-          "assets/fotos/ajustado/AJUSTADO_VERDE_FRENTE.avif",
-          "assets/fotos/ajustado/AJUSTADO_VERDE_ATRAS.avif",
-        ],
-      },
-    ],
   },
   {
     id: 3,
@@ -91,42 +19,6 @@ export const pantalones_general = [
     nombre: "Pantalón Cargo",
     precio: 320,
     imagen: "assets/fotos/cargo/CARGO_GRIS_CLARO_FRENTE.avif",
-    imagenes: [
-      {
-        azul: [
-          "assets/fotos/cargo/CARGO_AZUL_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_AZUL_ATRAS.avif",
-        ],
-        besh: [
-          "assets/fotos/cargo/CARGO_BESH_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_BESH_ATRAS.avif",
-        ],
-        cocoa: [
-          "assets/fotos/cargo/CARGO_COCOA_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_COCOA_ATRAS.avif",
-        ],
-        gris_claro: [
-          "assets/fotos/cargo/CARGO_GRIS_CLARO_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_GRIS_CLARO_ATRAS.avif",
-        ],
-        gris_oscuro: [
-          "assets/fotos/cargo/CARGO_GRIS_OSCURO_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_GRIS_OSCURO_ATRAS.avif",
-        ],
-        kaki: [
-          "assets/fotos/cargo/CARGO_KAKI_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_KAKI_ATRAS.avif",
-        ],
-        negro: [
-          "assets/fotos/cargo/CARGO_NEGRO_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_NEGRO_ATRAS.avif",
-        ],
-        verde: [
-          "assets/fotos/cargo/CARGO_VERDE_FRENTE.avif",
-          "assets/fotos/cargo/CARGO_VERDE_ATRAS.avif",
-        ],
-      },
-    ],
   },
   {
     id: 4,
@@ -134,42 +26,6 @@ export const pantalones_general = [
     nombre: "Pantalón Semirecto",
     precio: 320,
     imagen: "assets/fotos/semi/SEMI_AZUL_FRENTE.avif",
-    imagenes: [
-      {
-        azul: [
-          "assets/fotos/semi/SEMI_AZUL_FRENTE.avif",
-          "assets/fotos/semi/SEMI_AZUL_ATRAS.avif",
-        ],
-        besh: [
-          "assets/fotos/semi/SEMI_BESH_FRENTE.avif",
-          "assets/fotos/semi/SEMI_BESH_ATRAS.avif",
-        ],
-        cocoa: [
-          "assets/fotos/semi/SEMI_COCOA_FRENTE.avif",
-          "assets/fotos/semi/SEMI_COCOA_ATRAS.avif",
-        ],
-        gris_claro: [
-          "assets/fotos/semi/SEMI_GRIS_CLARO_FRENTE.avif",
-          "assets/fotos/semi/SEMI_GRIS_CLARO_ATRAS.avif",
-        ],
-        gris_oscuro: [
-          "assets/fotos/semi/SEMI_GRIS_OSCURO_FRENTE.avif",
-          "assets/fotos/semi/SEMI_GRIS_OSCURO_ATRAS.avif",
-        ],
-        kaki: [
-          "assets/fotos/semi/SEMI_KAKI_FRENTE.avif",
-          "assets/fotos/semi/SEMI_KAKI_ATRAS.avif",
-        ],
-        negro: [
-          "assets/fotos/semi/SEMI_NEGRO_FRENTE.avif",
-          "assets/fotos/semi/SEMI_NEGRO_ATRAS.avif",
-        ],
-        verde: [
-          "assets/fotos/semi/SEMI_VERDE_FRENTE.avif",
-          "assets/fotos/semi/SEMI_VERDE_ATRAS.avif",
-        ],
-      },
-    ],
   },
   {
     id: 5,
@@ -177,42 +33,6 @@ export const pantalones_general = [
     nombre: "Pantalón Jogger",
     precio: 320,
     imagen: "assets/fotos/jogger/JOGGER_KAKI_FRENTE.avif",
-    imagenes: [
-      {
-        azul: [
-          "assets/fotos/jogger/JOGGER_AZUL_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_AZUL_ATRAS.avif",
-        ],
-        besh: [
-          "assets/fotos/jogger/JOGGER_BESH_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_BESH_ATRAS.avif",
-        ],
-        cocoa: [
-          "assets/fotos/jogger/JOGGER_COCOA_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_COCOA_ATRAS.avif",
-        ],
-        gris_claro: [
-          "assets/fotos/jogger/JOGGER_GRIS_CLARO_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_GRIS_CLARO_ATRAS.avif",
-        ],
-        gris_oscuro: [
-          "assets/fotos/jogger/JOGGER_GRIS_OSCURO_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_GRIS_OSCURO_ATRAS.avif",
-        ],
-        kaki: [
-          "assets/fotos/jogger/JOGGER_KAKI_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_KAKI_ATRAS.avif",
-        ],
-        negro: [
-          "assets/fotos/jogger/JOGGER_NEGRO_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_NEGRO_ATRAS.avif",
-        ],
-        verde: [
-          "assets/fotos/jogger/JOGGER_VERDE_FRENTE.avif",
-          "assets/fotos/jogger/JOGGER_VERDE_ATRAS.avif",
-        ],
-      },
-    ],
   },
 ];
 
