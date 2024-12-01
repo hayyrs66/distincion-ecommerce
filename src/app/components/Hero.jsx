7d6809eb262bbf3f388e833fb9e2af1258bfb890
Your Name <rayrtsx@proton.me>
2024-12-01 13:25:57 -0600

    Refactor Hero component to use a tighter font weight for the Distinción link

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           </h2>
           <Link
             href="/ropa"
-            className="flex items-center gap-2 text-black font-semibold text-xl bg-white w-fit px-4 py-2 rounded-md"
+            className="flex items-center gap-2 text-black font-medium text-xl bg-white w-fit px-4 py-2 rounded-md"
           >
             Compra y Explora
             <svg
