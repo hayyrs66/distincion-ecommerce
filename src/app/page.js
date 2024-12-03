0cbb52152bc7aead01ba2931311c3fa63c7f03c3
Your Name <rayrtsx@proton.me>
2024-12-02 20:05:05 -0600

    Refactor page component to add Showcase section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Showcase from "./components/Showcase";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Hero/>  
+        <Showcase/>  
         <About/>
       </main>
     </div>
