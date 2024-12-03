64ac70763af49335ca08bee684259f1c04e8dbb1
hayyrs66 <rayrtsx@proton.me>
2024-12-02 23:42:04 -0600

    Refactor Hero component layout and enhance styling with gradient background and responsive design

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,37 +1,48 @@
-import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="w-full relative h-screen max-h-[550px] bg-no-repeat bg-cover px-4 bg-top">
-      <div className="w-auto -z-10 h-full absolute top-1/2 left-4 right-4 -translate-y-1/2 bg-[#f3f3f3]" />
-      <div className="w-full h-full flex items-center px-24">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col justify-center gap-3">
-            <span className="text-black text-3xl max-w-[800px] text-balance tracking-tight font-normal">
+    <section className="relative w-full h-screen bg-no-repeat bg-cover bg-top px-4">
+      {/* Fondo gris detrás */}
+      <div className="absolute inset-0 -z-10 bg-gradient-to-r from-indigo-50 to-indigo-100"></div>
+
+      {/* Contenido principal */}
+      <div className="container flex h-full items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full h-full items-center">
+          {/* Columna de texto */}
+          <div className="flex flex-col gap-4 h-full w-full justify-center px-12">
+            <span className="text-black text-4xl text-balance tracking-tight font-normal">
               Clásicos Exclusivos
             </span>
-            <h2 className="text-black text-5xl max-w-[800px] text-balance tracking-tight font-semibold">
+            <h2 className="text-black bg-clip-text text-transparent bg-gradient-to-r uppercase from-black to-black/70 text-6xl w-full tracking-tight font-semibold leading-tight">
               Colección de Hombres
             </h2>
-            <p className="text-black text-xl max-w-[800px] text-balance tracking-tight font-light">
+            <p className="text-black text-base md:text-xl max-w-[800px] text-balance tracking-tight font-normal">
               Ahorra hasta el 40%
             </p>
             <Link
-            href="/ropa"
-            className="flex items-center gap-2 text-white font-normal text-sm bg-black w-fit px-4 py-2 rounded-md"
-          >
-            Explorar Ahora
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="filter fill-white"
+              href="/ropa"
+              className="flex items-center gap-2 text-white font-normal text-sm bg-black w-fit px-4 py-2 rounded-md hover:bg-gray-800 transition-all"
             >
-              <path d="m11.293 17.293 1.414 1.414L19.414 12l-6.707-6.707-1.414 1.414L15.586 11H6v2h9.586z"></path>
-            </svg> */}
-          </Link>
+              Explorar Ahora
+            </Link>
+          </div>
+
+          {/* Columna de imagen */}
+          <div className="relative w-full h-full flex justify-center items-center">
+            <Image
+              src="/assets/content/hero.png"
+              alt="Pantalón cargo y ajustado"
+              width={500}
+              height={500}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="rounded-md transition-all duration-500"
+              style={{
+                objectFit: "cover",
+                objectPosition: "center",
+              }}
+            />
           </div>
         </div>
       </div>
