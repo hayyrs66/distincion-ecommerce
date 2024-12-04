2ea8a4d895526c32746640b7cbb73060376e82d8
hayyrs66 <rayrtsx@proton.me>
2024-12-03 22:00:49 -0600

    Add Newsletter and Services components to the Home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,19 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Showcase from "./components/Showcase";
 import MostSold from "./components/MostSold";
+import Services from "./components/Services";
+import Newsletter from "./components/Newsletter";
 
 export default function Home() {
   return (
     <div>
       <main>
-        <Hero/>  
-        <Showcase/>  
+        <Hero />
+        <Showcase />
         <MostSold />
-        <About/>
+        <Newsletter />
+        <Services />
+        <About />
       </main>
     </div>
   );
