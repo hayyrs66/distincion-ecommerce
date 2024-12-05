7c1d0240c2af59890febc597d7a74c686b76f069
hayyrs66 <rayrtsx@proton.me>
2024-12-04 23:41:16 -0600

    Remove Poppins font integration from layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,6 @@ import { CartProvider } from "./context/CartProvider";
 import { Toaster } from "../components/ui/toaster";
 import Footer from "./components/Footer";
 
-import { Poppins } from "next/font/google";
-
-const roboto = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  display: "swap",
-});
-
 export const metadata = {
   title: "Distinción Guatemala",
   description: "Distinción, tienda de ropa en Guatemala",
@@ -21,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} font-sans`}>
+      <body>
         <CartProvider>
           <Header />
           {children}
