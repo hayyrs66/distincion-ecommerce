e85686a809aaf2ed3da800992a111978c496ddf5
hayyrs66 <rayrtsx@proton.me>
2024-12-04 20:45:26 -0600

    Enhance Services component with new icon and updated content for clarity

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -1,10 +1,10 @@
-import { Package, Headset, CreditCard } from "lucide-react";
+import { Package, Headset, CreditCard, ShieldCheck } from "lucide-react";
 
 const Services = () => {
   return (
-    <section className="w-full px-6 py-12 bg-white">
+    <section className="w-full px-6 py-12">
       <div className="w-full gap-2 grid grid-cols-4">
-        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-lg border-2 border-black/15 bg-white">
+        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-md ">
           <Package size={38} />
           <h3 className="font-semibold text-base leading-1 tracking-tight">
             Envío Gratis
@@ -13,31 +13,29 @@ const Services = () => {
             Envío gratis todo diciembre.
           </p>
         </div>
-        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-lg border-2 border-black/15 bg-white ">
+        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-md  ">
           <Headset size={38} />
           <h3 className="font-semibold text-base leading-1 tracking-tight">
             Soporte
           </h3>
+          <p className="font-normal tracking-tight">Soporte 8 horas al día.</p>
+        </div>
+        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-md  ">
+          <CreditCard size={38} />
+          <h3 className="font-semibold text-base leading-1 tracking-tight">
+            Pago Flexible
+          </h3>
           <p className="font-normal tracking-tight">
-            Soporte 8 horas al día.
+            Pago online seguro.
           </p>
         </div>
-        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-lg border-2 border-black/15 bg-white ">
-          <CreditCard size={38} />
+        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-md  ">
+          <ShieldCheck size={38} />
           <h3 className="font-semibold text-base leading-1 tracking-tight">
-            Pago Flexible
+            Calidad Asegurada
           </h3>
           <p className="font-normal tracking-tight">
-            Paga con cualquier tarjeta.
-          </p>  
-        </div>
-        <div className="flex flex-col py-12 w-full h-full items-center gap-2 rounded-lg border-2 border-black/15 bg-white ">
-          <CreditCard size={38} />
-          <h3 className="font-semibold text-base leading-1 tracking-tight">
-            Pago Flexible
-          </h3>
-          <p className="font-normal tracking-tight">
-            Paga con cualquier tarjeta.
+            Garantía de calidad.
           </p>
         </div>
       </div>
