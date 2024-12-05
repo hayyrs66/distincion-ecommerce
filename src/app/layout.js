9f1d0938c70bf8e0a3e7d975ec435d8ad654b6d3
hayyrs66 <rayrtsx@proton.me>
2024-12-04 23:39:23 -0600

    Integrate Poppins font into layout component and apply to body class

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,17 @@
 import "./globals.css";
 import Header from "./components/Header";
 import { CartProvider } from "./context/CartProvider";
+
 import { Toaster } from "../components/ui/toaster";
 import Footer from "./components/Footer";
+
+import { Poppins } from "next/font/google";
+
+const roboto = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "Distinción Guatemala",
@@ -12,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${roboto.className} font-sans`}>
         <CartProvider>
           <Header />
           {children}
