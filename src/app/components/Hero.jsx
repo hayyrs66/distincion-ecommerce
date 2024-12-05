6fdd8ec297e15eb4fe785f2d00c9ff2c63e850d1
hayyrs66 <rayrtsx@proton.me>
2024-12-04 20:45:17 -0600

    Refactor Hero component for improved layout and typography

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,49 +1,31 @@
-import Link from "next/link";
-import Image from "next/image";
+import localFont from "next/font/local";
+
+const leMurmure = localFont({
+  src: "../fonts/lemurmure/LeMurmure-Regular.woff2",
+});
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen bg-no-repeat bg-cover bg-top">
-      {/* Fondo gris detrás */}
-      <div className="absolute inset-0 -z-10"></div>
+    <section className="relative w-full flex justify-center items-center h-[calc(100vh_+_5rem)] bg-[#838383]">
       {/* Contenido principal */}
-      <div className="w-full flex h-full items-center bg-indigo-100">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full h-full items-center">
-          {/* Columna de texto */}
-          <div className="flex flex-col gap-4 h-full w-full justify-center px-12">
-            <span className="text-black text-4xl text-balance tracking-tight font-normal">
-              Clásicos Exclusivos
-            </span>
-            <h2 className="text-black bg-clip-text text-transparent bg-gradient-to-r uppercase from-black to-black/70 text-6xl w-full tracking-tight font-semibold leading-tight">
-              Colección de Hombres
-            </h2>
-            <p className="text-black text-base md:text-xl max-w-[800px] text-balance tracking-tight font-normal">
-              Ahorra hasta el 40%
-            </p>
-            <Link
-              href="/ropa"
-              className="flex items-center gap-2 text-white font-normal text-sm bg-black w-fit px-4 py-2 rounded-md hover:bg-gray-800 transition-all"
-            >
-              Explorar Ahora
-            </Link>
-          </div>
-
-          {/* Columna de imagen */}
-          <div className="relative w-full h-full flex justify-center items-center">
-            <Image
-              src="/assets/content/hero.png"
-              alt="Pantalón cargo y ajustado"
-              width={500}
-              height={500}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className="rounded-md transition-all duration-500"
-              style={{
-                objectFit: "cover",
-                objectPosition: "center",
-              }}
-            />
-          </div>
+      <div className="w-full relative h-full flex justify-center items-center">
+        <div className="w-full h-full flex justify-center items-center -translate-y-28">
+          <h2
+            className={`${leMurmure.className} text-[30rem] tracking-tight font-medium text-white `}
+          >
+            Cole
+          </h2>
+          <h2 className={`${leMurmure.className} text_outline text-[30rem]`}>
+            cción
+          </h2>
         </div>
+        <img
+          src="/person_hero.png"
+          alt=""
+          width={450}
+          height={450}
+          className="absolute bottom-0"
+        />
       </div>
     </section>
   );
