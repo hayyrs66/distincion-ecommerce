cbf7f9639321d02a4f270bff52680d156c92d1a8
hayyrs66 <rayrtsx@proton.me>
2024-12-04 23:10:44 -0600

    Remove unused localFont import and related code from Hero component

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,8 +1,8 @@
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 
-const leMurmure = localFont({
-  src: "../fonts/lemurmure/LeMurmure-Regular.woff2",
-});
+// const leMurmure = localFont({
+//   src: "../fonts/lemurmure/LeMurmure-Regular.woff2",
+// });
 
 const Hero = () => {
   return (
@@ -11,12 +11,12 @@ const Hero = () => {
       <div className="w-full relative h-full flex justify-center items-center">
         <div className="w-full h-full flex flex-wrap justify-center items-center">
           <h2
-            className={`${leMurmure.className} tracking-tight font-medium text-white text-clamp_hero`}
+            className={` tracking-tight font-medium text-white text-clamp_hero`}
           >
             Cole
           </h2>
           <h2
-            className={`${leMurmure.className} tracking-tight font-medium text-white text_outline text-clamp_hero`}
+            className={` tracking-tight font-medium text-white text_outline text-clamp_hero`}
           >
             cción
           </h2>
