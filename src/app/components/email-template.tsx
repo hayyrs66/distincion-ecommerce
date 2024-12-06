eb5d3ebbb0ba6a9eff8d4cbc80cf346efbc3f66b
hayyrs66 <rayrtsx@proton.me>
2024-12-05 21:03:34 -0600

    Update email template greeting to include personalized message

--- src/app/components/email-template.tsx
+++ src/app/components/email-template.tsx
@@ -8,6 +8,6 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
 }) => (
   <div>
-    <h1>Welcome, {firstName}!</h1>
+    <h1>Comeme la chucha {firstName}</h1>
   </div>
 );
