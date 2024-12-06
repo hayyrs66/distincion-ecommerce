b56fb34e431f42fdb682e8000511c592e0b319f9
hayyrs66 <rayrtsx@proton.me>
2024-12-05 21:04:48 -0600

    Update sender email address in send route for email notifications

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -8,7 +8,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: "Distinción <onboarding@resend.dev>",
+      from: "Distinción <pedidos@distincion.shop>",
       to: ["contacto@distincion.shop"],
       subject: "Compra en línea",
       react: EmailTemplate({ firstName: "Daniel" }),
