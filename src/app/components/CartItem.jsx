ecae67ebe716a248749571a0d19744d6dbe76e41
hayyrs66 <rayrtsx@proton.me>
2024-12-07 21:59:00 -0600

    Refactor CartItem component: replace icons with Lucide, enhance layout, and improve button styles

--- src/app/components/CartItem.jsx
+++ src/app/components/CartItem.jsx
@@ -1,6 +1,4 @@
-import Down from "./icons/Down";
-import Trash from "./icons/Trash";
-import Up from "./icons/Up";
+import { X } from "lucide-react";
 
 const colorNames = {
   azul: "Azul",
@@ -17,7 +15,7 @@ const CartItem = ({ item, onIncrease, onDecrease, onRemove }) => {
   const translatedColor = colorNames[item.color] || item.color;
 
   return (
-    <article className="border-b border-b-black/10 p-4 rounded-md flex justify-between items-center">
+    <article className="p-4 rounded-md flex justify-between items-center">
       {/* Detalles del Producto */}
       <div className="flex gap-4 items-center">
         <div>
@@ -34,7 +32,7 @@ const CartItem = ({ item, onIncrease, onDecrease, onRemove }) => {
           <p className="text-sm text-gray-500">Q{item.price.toFixed(2)}</p>
           <div className="flex items-center gap-2 mt-1">
             <p className="text-sm">
-              {item.size} - {translatedColor}
+              {item.size} — {translatedColor}
             </p>
             <div
               className={`w-4 h-4 rounded-full border border-gray-300 flex items-center justify-center`}
@@ -50,29 +48,29 @@ const CartItem = ({ item, onIncrease, onDecrease, onRemove }) => {
 
       {/* Controles de Cantidad y Eliminar */}
       <div className="flex items-center gap-2">
-        <div className="flex items-center">
+        <div className="flex items-center border rounded-sm">
           <button
             onClick={onDecrease}
-            className="bg-gray-200 rounded-md hover:bg-gray-300 transition-colors active:bg-green-200"
+            className="px-2 text-xl transition-colors active:bg-green-200"
             aria-label="Disminuir cantidad"
           >
-            <Down />
+            -
           </button>
           <span className="px-2 text-sm">{item.quantity}</span>
           <button
             onClick={onIncrease}
-            className="bg-gray-200 rounded-md hover:bg-gray-300 transition-colors active:bg-green-200"
+            className=" px-2 text-xl transition-colors active:bg-green-200"
             aria-label="Aumentar cantidad"
           >
-            <Up />
+            +
           </button>
         </div>
         <button
           onClick={onRemove}
-          className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-md"
+          className="bg-[#F6F6F6] hover:bg-[#eeeeee] px-2 py-1 rounded-md transition-colors"
           aria-label="Eliminar artículo"
         >
-          <Trash />
+          <X color="#5C5F6A" />
         </button>
       </div>
     </article>
