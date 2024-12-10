921d04e323323e670e5320bd76e435856b8835d5
Your Name <rayrtsx@proton.me>
2024-12-10 12:39:10 -0600

    Refactor Cart component: Update className for responsive behavior

--- src/app/components/Cart.jsx
+++ src/app/components/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = ({ openCart, closeCart }) => {
 
   return (
     <section
-      className={`fixed top-0 right-0 sm:w-full md:w-[400px] h-full drop-shadow-xl bg-white z-20 transition-transform duration-300 ${
+      className={`fixed top-0 right-0 sm:w-full md:w-[400px] w-full h-full drop-shadow-xl bg-white z-20 transition-transform duration-300 ${
         openCart ? "translate-x-0" : "translate-x-full"
       } flex flex-col`}
     >
