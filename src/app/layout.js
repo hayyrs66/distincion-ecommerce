ba00253e0d40e68d48a101f242bdfc218d07bd11
Your Name <rayrtsx@proton.me>
2024-12-09 23:18:48 -0600

    Update lang attribute in RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" className="dark">
       <body className={inter.className}>
         <CartProvider>
           <Header />
